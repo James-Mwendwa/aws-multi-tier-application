fc137da8b18953eccacccaa6799c2586eded2b80
James Mwendwa <mwendwajames32@gmail.com>
2025-08-16 10:17:41 +0300

    update modules

--- infra/main.tf
+++ infra/main.tf
@@ -4,3 +4,15 @@ module "ecr" {
   backend_app_repo_name = var.backend_app_repo_name
   aws_region            = var.aws_region
 }
+
+module "eks" {
+  source = "./eks"
+}
+
+module "vpc" {
+  source              = "./vpc"
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  availability_zone   = var.availability_zone
+}
