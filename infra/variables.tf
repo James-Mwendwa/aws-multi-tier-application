2ef807d907837c5a099f95cc62be63fc6f3bcc88
James Mwendwa <mwendwajames32@gmail.com>
2025-08-16 10:18:29 +0300

    feat: add network infra variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,3 +12,24 @@ variable "backend_app_repo_name" {
   description = "Name of the ECR repository for the backend application"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "value for private subnet cidr"
+  type        = list(string)
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnets"
+  type        = list(string)
+
+}
