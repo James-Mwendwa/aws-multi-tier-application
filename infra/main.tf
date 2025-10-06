1744488eb0fca8e065b32e56f4bf2940e9039f8c
James Mwendwa <mwendwajames32@gmail.com>
2025-10-06 23:04:13 +0300

    feat: eks module

--- infra/main.tf
+++ infra/main.tf
@@ -6,8 +6,14 @@ module "ecr" {
 }
 
 module "eks" {
-  source = "./eks"
+  source        = "./eks"
+  cluster_name  = var.cluster_name
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  public_subnet_ids  = module.vpc.public_subnet_ids
 }
+
 
 module "vpc" {
   source              = "./vpc"
